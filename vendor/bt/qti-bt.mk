dcb55d95d9cf146d984a08e0e8f6c462865b1ad9
clhex <100755123+clhexftw@users.noreply.github.com>
2022-12-11 03:15:03 -0500

    build 2.0

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -21,13 +21,13 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libldacBT_enc \
     libldacBT_abr \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
